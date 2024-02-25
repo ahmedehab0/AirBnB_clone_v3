ee7e52793d1debb91ada5a8daf4069b246e1462d
ahmedehab0 <ahmed.eh01@gmail.com>
2024-02-25 12:05:36 -0500

    fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,13 @@
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+host = getenv("HBNB_API_HOST", "0.0.0.0")
+port = getenv("HBNB_API_PORT", "5000")
+
 
 @app.teardown_appcontext
 def clear(exception):
@@ -20,5 +24,4 @@ def page_not_found(e):
 
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 5000,
-            threaded = True)
+    app.run(host, port, threaded = True, debug = True)
